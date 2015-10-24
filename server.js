cde4488a95027dfac7ae3afb6adb7f8f74dd0274
Mikael Jagekrans <micke.jagekrans@gmail.com>
2015-10-24 14:49:20 +0200

    Added index, styles and some logic

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 
 var port = 3000;
 
-app.get('/', function (req, res) {
+app.use(express.static(__dirname + '/www'));
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
 var server = app.listen(port, function () {
-
+  console.log("Server listening on port " + port);
 });
---
+++ www/js/gameFront.js
@@ -0,0 +1,3 @@
+window.goToGame = function (gameName) {
+  window.location.href = '/games/' + gameName + '.html';
+};
